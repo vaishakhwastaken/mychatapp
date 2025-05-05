6f8712010f27e05b6e973ad8546f662f12d1071b
Vaishakh V S <vaishakhvs2004@gmail.com>
2025-05-05 23:01:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState,useRef } from 'react';
 import './App.css';
+import {Auth} from './components/Auth';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room,setRoom] = useState(null);
 
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {	
+  return (
+    <div>
+      <Auth setIsAuth={setIsAuth}/>
+    </div>
+    );
+  }
+
+  return <div>
+    {room ? (
+      <div> Chat</div>
+    ): (
+    <div className='room'>
+      <label>Enter room name</label>
+      <input ref={roomInputRef} />
+      <button onClick={() => setRoom(roomInputRef.current.value)}>
+        Enter Chat
+        </button>
+      </div>
+      )}
+      </div>;
+}
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider} from "firebase/auth";	
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_UfIjQftwo9_eyj4HigvFbmCHIWCTQu8",
+  authDomain: "chatapp-33c1c.firebaseapp.com",
+  projectId: "chatapp-33c1c",
+  storageBucket: "chatapp-33c1c.firebasestorage.app",
+  messagingSenderId: "748946431622",
+  appId: "1:748946431622:web:72c2f1b74c69ad723a7b07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(app); // Google Auth Provider
